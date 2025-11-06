036208d174ee72ee5da0350980e5cee28c260503
RicercarG <yg2709@nyu.edu>
2025-11-06 14:41:55 -0500

    add support for project account in interactive runtime

--- chsdevice.sh
+++ chsdevice.sh
@@ -3,67 +3,92 @@
 script_dir=$(dirname "$(realpath "$0")")
 
 is_integer() {
-    [[ $1 =~ ^[0-9]+$ ]]
+  [[ $1 =~ ^[0-9]+$ ]]
 }
 
+GREEN='\033[0;32m'
+GRAY='\033[0;30m'
+NC='\033[0m' # No Color
 
 echo "Allocating device runtime"
 read -p "How many CPUs do you want? " num_cpu
 if ! is_integer "$num_cpu"; then
-    echo "Error: Input is not an integer."
-    exit 1
+  echo "Error: Input is not an integer."
+  exit 1
 fi
 if [ "$num_cpu" -ne 0 ]; then
-    read -p "How many GPUs do you want? (Type 0 to use CPU only): " num_gpu
-    if ! is_integer "$num_gpu"; then
-        echo "Error: Input is not an integer."
+  read -p "How many GPUs do you want? (Type 0 to use CPU only): " num_gpu
+  if ! is_integer "$num_gpu"; then
+    echo "Error: Input is not an integer."
+    exit 1
+  fi
+  if [ "$num_gpu" -ne 0 ]; then
+    echo "What GPU do you want":
+    options=("rtx8000" "v100" "a100" "h100")
+    select opt in "${options[@]}"; do
+      case $opt in
+      "rtx8000")
+        gpu_type="rtx8000"
+        break
+        ;;
+      "v100")
+        gpu_type="v100"
+        break
+        ;;
+      "a100")
+        gpu_type="a100"
+        break
+        ;;
+      "h100")
+        gpu_type="h100"
+        break
+        ;;
+      *)
+        echo "Invalid option"
         exit 1
-    fi
-    if [ "$num_gpu" -ne 0 ]; then
-        echo "What GPU do you want":
-        options=("rtx8000" "v100" "a100" "h100")
-        select opt in "${options[@]}"; do
-            case $opt in 
-                "rtx8000")
-                    gpu_type="rtx8000"
-                    break
-                    ;;
-                "v100")
-                    gpu_type="v100"
-                    break
-                    ;;
-                "a100")
-                    gpu_type="a100"
-                    break
-                    ;;
-                "h100")
-                    gpu_type="h100"
-                    break
-                    ;;
-                *) 
-                    echo "Invalid option"
-                    exit 1
-                    ;;
-            esac
-        done
-    fi
-    read -p "How much memory (GB) do you want? (Enter integer only): " mem
-    if ! is_integer "$mem"; then
-        echo "Error: Input is not an integer."
-        exit 1
-    fi
-    read -p "How long (hours) will you use these device? (Enter integer only): " time
-    if ! is_integer "$time"; then
-        echo "Error: Input is not an integer."
-        exit 1
-    fi
+        ;;
+      esac
+    done
+  fi
+  read -p "How much memory (GB) do you want? (Enter integer only): " mem
+  if ! is_integer "$mem"; then
+    echo "Error: Input is not an integer."
+    exit 1
+  fi
+  read -p "How long (hours) will you use these device? (Enter integer only): " time
+  if ! is_integer "$time"; then
+    echo "Error: Input is not an integer."
+    exit 1
+  fi
 
-    if [ "$num_gpu" -ne 0 ]; then
-        srun --nodes=1 --cpus-per-task="$num_cpu" --mem="$mem"GB --time="$time":00:00 --gres=gpu:"$gpu_type":"$num_gpu" --pty /bin/bash
-    else
-        srun --nodes=1 --cpus-per-task="$num_cpu" --mem="$mem"GB --time="$time":00:00 --pty /bin/bash
-    fi
+  # load the account if possible
+  if [ -f "$script_dir/account.txt" ]; then
+    account=$(cat account.txt)
+  else
+    account=""
+  fi
+
+  echo -e "${GREEN}Requesting device with project account $account ${NC}"
+
+  # write the base command
+  cmd="srun --nodes=1 --cpus-per-task="$num_cpu" --mem="$mem"GB --time="$time":00:00"
+
+  # request for gpu if necessary
+  if [ "$num_gpu" -ne 0 ]; then
+    cmd="$cmd --gres=gpu:"$gpu_type":"$num_gpu""
+  fi
+
+  # add account name if there's any
+  if [ ${#account} -ne 0 ]; then
+    cmd="$cmd --account="$account""
+  fi
+
+  # add interactive flag
+  cmd="$cmd --pty /bin/bash"
+
+  # run the final command to ask for devices
+  eval "$cmd"
 
 else
-    echo "Device allocation is skipped."
+  echo "Device allocation is skipped."
 fi
--- setup.sh
+++ setup.sh
@@ -7,47 +7,49 @@ ssl_cert='export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt'
 
 # Check if the line already exists in .bashrc
 if ! grep -qF "$ssl_cert" ~/.bashrc; then
-    # Append the line to .bashrc if it doesn't exist
-    echo "$ssl_cert" >> ~/.bashrc
-    echo "Added ssl certificate file to .bashrc"
+  # Append the line to .bashrc if it doesn't exist
+  echo "$ssl_cert" >>~/.bashrc
+  echo "Added ssl certificate file to .bashrc"
 else
-    echo "ssl certificate file already exists in .bashrc"
+  echo "ssl certificate file already exists in .bashrc"
 fi
 
-
 if ! echo "$PATH" | grep -q "$script_dir"; then
-    echo "export PATH=\$PATH:$script_dir" >> ~/.bashrc
-    source ~/.bashrc
-    echo "NYU-Greene-HPC-Cheatsheet Directory added to PATH"
+  echo "export PATH=\$PATH:$script_dir" >>~/.bashrc
+  source ~/.bashrc
+  echo "NYU-Greene-HPC-Cheatsheet Directory added to PATH"
 else
-    echo "NYU-Greene-HPC-Cheatsheet Directory already in PATH"
+  echo "NYU-Greene-HPC-Cheatsheet Directory already in PATH"
 fi
 
 chmod +rx "$script_dir/chsdevice.sh"
 chmod +rx "$script_dir/chslauncher.sh"
 
 if ! grep -Fxq "alias cdv='$script_dir/chsdevice.sh'" "$bashrc_file"; then
-    echo "alias cdv='$script_dir/chsdevice.sh'" >> "$bashrc_file"
-    echo "Alias cdv for allocating device runtime is added to ~/.bashrc"
+  echo "alias cdv='$script_dir/chsdevice.sh'" >>"$bashrc_file"
+  echo "Alias cdv for allocating device runtime is added to ~/.bashrc"
 else
-    echo "Alias cdv already exsits in ~/.bashrc"
+  echo "Alias cdv already exsits in ~/.bashrc"
 fi
 
 if ! grep -Fxq "alias clc='$script_dir/chslauncher.sh'" "$bashrc_file"; then
-    echo "alias clc='$script_dir/chslauncher.sh'" >> "$bashrc_file"
-    echo "Alias clc for launching singularity environment is added to ~/.bashrc"
+  echo "alias clc='$script_dir/chslauncher.sh'" >>"$bashrc_file"
+  echo "Alias clc for launching singularity environment is added to ~/.bashrc"
 else
-    echo "Alias clc already exsits in ~/.bashrc"
+  echo "Alias clc already exsits in ~/.bashrc"
 fi
 
 if ! grep -Fxq "alias se='source /ext3/env.sh'" "$bashrc_file"; then
-    echo "alias se='source /ext3/env.sh'" >> "$bashrc_file"
-echo "Alias se for source /ext3/env.sh is added to ~/.bashrc"
+  echo "alias se='source /ext3/env.sh'" >>"$bashrc_file"
+  echo "Alias se for source /ext3/env.sh is added to ~/.bashrc"
 else
-    echo "Alias se already exsits in ~/.bashrc"
+  echo "Alias se already exsits in ~/.bashrc"
 fi
 
-# apply changes
-# source "$bashrc_file"
+read -p "
+(Optional) Type in your project account for priority device usage (e.g. pr_000_tandon_advanced)
+Press ENTER and leave it blank if you don't know about it.
+" project_account
+echo "$project_account" >"$script_dir/account.txt"
 
 echo "NYU-Greene-HPC-Cheatsheet lazy launcher setup complete."
